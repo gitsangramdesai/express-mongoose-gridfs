010e76faa286db4c72d4a8d9e89c79972e5ba177
Sangram Desai <mail.sangram.desai@gmail.com>
2024-04-29 16:49:23 +0530

    added condition on download

--- routes/demo.js
+++ routes/demo.js
@@ -3,27 +3,30 @@ let router = express.Router();
 let FoodItem = require("../models/foodItem").FoodItem;
 let FoodItemSchema = require("../models/foodItem").FoodItemSchema;
 let mongoose = require("mongoose");
-let {upload} = require("../upload");
-const path = require('path')
-const fs = require('fs')
-let {bucket} = require("../connection")
+let { upload } = require("../upload");
+const path = require("path");
+const fs = require("fs");
+let { bucket, connection } = require("../connection");
 
 /* insert a new food item */
-router.post("/insert",upload.single('resume'), async function (req, res, next) {
+router.post(
+  "/insert",
+  upload.single("resume"),
+  async function (req, res, next) {
     let foodItemName = req.body.name;
     let foodItemDesc = req.body.description;
     let foodItemPrice = req.body.price;
 
-    console.log("Files",)
+    console.log("Files");
 
     try {
       let foodItem = await FoodItem.create({
         name: foodItemName,
         description: foodItemDesc,
         price: foodItemPrice,
-        picture:req.file.filename
+        picture: req.file.filename,
       });
-   
+
       res.json({
         message: "food item added successfully",
         error: "",
@@ -38,14 +41,17 @@ router.post("/insert",upload.single('resume'), async function (req, res, next) {
         success: false,
       });
     }
+  }
+);
+
+router.get("/image/:filename", async (req, res) => {
+  let foods = await FoodItem.find({ picture: req.params.filename })  
+  if(foods.length){
+       bucket.openDownloadStreamByName(req.params.filename).
+       pipe(res);
+  }else{
+    res.json({"msg":"no file found"})
+  }
 });
-
-
-
-router.get("/image/:filename", (req, res) => {
-  bucket.openDownloadStreamByName(req.params.filename).
-  pipe(res);
-});
-
 
 module.exports = router;
